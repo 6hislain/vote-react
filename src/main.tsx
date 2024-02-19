e242691cabf81d42abbc8a6b4816d5f6e358ab2a
6hislain <ghislainmike80@gmail.com>
2024-02-19 20:50:36 +0200

    fix redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import "./index.css";
 import React from "react";
 import routes from "./routes";
-import { store } from "./store";
-import { Provider } from "react-redux";
+// import { store } from "./store";
+// import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={routes} />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <RouterProvider router={routes} />
+    {/* </Provider> */}
   </React.StrictMode>
 );
